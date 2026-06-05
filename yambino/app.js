516b34b52a62875fdec5f0c881b1b57115b91836
Neb <np@head2toes.org>
2026-06-04 23:24:25 -0700

    yambino sw.js registration

--- yambino/app.js
+++ yambino/app.js
@@ -402,7 +402,7 @@ function draw() {
 
 // Service Worker registration
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js')
+    navigator.serviceWorker.register('./sw.js')
         .then(function() {
             console.log('Service Worker Registered');
         });
